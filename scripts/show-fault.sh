6d083dafc11e2aa31680352d1e3b96b73396cba0
bdebruijn <bdebruijn@luminize.nl>
2018-06-29 10:54:20 +0200

    update script output

--- scripts/show-fault.sh
+++ scripts/show-fault.sh
@@ -2,5 +2,5 @@
 
 # see fault and error code
 JOINT=$1
-echo "fault ? $(halcmd getp lcec.0.$((JOINT - 1)).fault)"
-echo "error : $(halcmd getp lcec.0.$((JOINT - 1)).error-code)"
+echo "Check if lcec.0.$((JOINT - 1)).fault: $(halcmd getp lcec.0.$((JOINT - 1)).fault)"
+echo "The error code is      : $(halcmd getp lcec.0.$((JOINT - 1)).error-code)"
